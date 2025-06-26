7c5f10d8ac1bde9aea016ebcfd0069c2a0087da3
David Abedu Ewusi <ewusidavid8@gmail.com>
2025-06-26 02:36:56 +0000

    Completed Binary Search List Exercise and tested with Jest

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo: number = 0;
+    let hi: number = haystack.length;
 
+    while (lo < hi) {
+        const mid = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[mid];
+
+        if (needle === value) {
+            return true
+        } else if (needle > value) {
+            lo = mid + 1;
+        } else {
+            hi = mid;
+        }
+    }
+    return false;
 }
